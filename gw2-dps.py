b0228b4e67e2e904c82da827708f42b7fbc81553
Mike Renfro <mike+github@renf.ro>
2025-08-16 13:04:46 -0500

    refactor out functions specific to dps.report

--- gw2-dps.py
+++ gw2-dps.py
@@ -1,46 +1,12 @@
 import argparse
-import json
-import math
-import urllib.request
 
-import pandas as pd
-
-
-def read_json_url(u):
-    dps_url = f"https://dps.report/getJson?permalink={u}"
-    with urllib.request.urlopen(dps_url) as url:
-        data = json.load(url)
-    return data
-
-
-def create_mechanics_df(d):
-    accounts = [p['account'] for p in d['players']]
-    names = [p['name'] for p in d['players']]
-    mechanics = [m['name'] for m in d['mechanics']]
-
-    df = pd.DataFrame(index=accounts, columns=mechanics)
-    for m in mechanics:
-        df[m] = df[m].astype('object')
-    df['Name'] = names
-    for m in d['mechanics']:
-        for md in m['mechanicsData']:
-            account = df.index[df['Name'] == md['actor']].tolist()[0]
-            if isinstance(df.at[account, m['name']], float) and \
-                    math.isnan(df.at[account, m['name']]):
-                df.at[account, m['name']] = md['time']
-            elif isinstance(df.at[account, m['name']], int):
-                df.at[account, m['name']] = [df.at[account, m['name']],
-                                             md['time']]
-            else:
-                df.at[account, m['name']].append(md['time'])
-    return df
-
+import dps_report as dr
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('url', help='dps.report url')
     args = parser.parse_args()
 
-    data = read_json_url(args.url)
-    mechanics_df = create_mechanics_df(data)
+    data = dr.read_json_url(args.url)
+    mechanics_df = dr.create_mechanics_df(data)
     print(mechanics_df)
